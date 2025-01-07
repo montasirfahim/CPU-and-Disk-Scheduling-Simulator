0d0e5455a05e0a21f64cdb3ffed144fbbb49ce09
Fahim Montasir <montasirtuhin@gmail.com>
2025-01-07 09:56:57 +0600

    added operations class

---
+++ src/SchedulersOperations.java
@@ -0,0 +1,9 @@
+public class SchedulersOperations {
+    public static void cpuAlgorithms(){
+        System.out.println("okay");
+    }
+
+    public static void diskAlgorithms(){
+        System.out.println("okay2");
+    }
+}
